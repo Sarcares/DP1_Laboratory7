c4ad91d7f955dc8679fbbd11a39296ba3e1c997f
Sarcares <s175637@studenti.polito.it>
2016-06-05 15:04:05 +0200

    file updated

--- lab7_1_1_A.php
+++ lab7_1_1_A.php
@@ -77,16 +77,17 @@
 		<p>Unfortunately your browser <strong>does not support Javascript</strong> and some functionality are not available!</p>
 	</noscript>
 	<footer>
-		<TABLE ID="Outer" ><TR>
-			<TH> <img src="http://security.polito.it/img/polito.gif" alt="PoliTo's Logo"> </TH>
-			<TH><div>This website was developed by LM Production&reg;, all rights are reserved.</div>
-				<TABLE ID="Inner" style="border-spacing: 10px; bor" > <TR>
-					<TH><a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="CSS Valido!" /></a></TH>
-					<TH><a href="https://it.linkedin.com/pub/luca-mannella/a1/859/9a5"> The author </a></TH>
-					<TH><a href="mailto:luca.mannella@outlook.it?Subject=Information%20about%20website">Contact me</a></TH>
-					<TH><a href="http://validator.w3.org/check?uri=referer"> <img src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Strict" height="31" width="88"></a></TH>
-				</TR></TABLE>
-			</TH>
-		</TR></TABLE>
+		<TABLE ID="Outer" > <TR>
+				<TH> <img src="http://security.polito.it/img/polito.gif" alt="PoliTo's Logo"> </TH>
+				<TH> <div>This website was developed by Luca Mannella&reg;, all rights are reserved.</div>
+					<TABLE ID="Inner" style="border-spacing: 10px;" > <TR>
+							<TH><a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="CSS Valido!" /></a></TH>
+							<TH><a href="https://it.linkedin.com/pub/luca-mannella/a1/859/9a5"> The author </a></TH>
+							<th><em><a href="mailto:luca.mannella@studenti.polito.it?Subject=Bug%20or%20Problem%20found!">Contact me</a></em></th>
+							<TH><i><a href="http://lukeman.altervista.org">Go back to the Home</a></i></TH>
+							<TH><a href="http://validator.w3.org/check?uri=referer"> <img src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Strict" height="31" width="88"></a></TH>
+					</TR> </TABLE>
+				</TH>
+		</TR> </TABLE>
 	</footer>
 </html>
